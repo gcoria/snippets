4b517a684e65cfb6de01cb5348d314d798be39aa
Gaston <gaston.e.coria@gmail.com>
2021-09-02 17:48:09 -0300

    next number in array

--- ruby/snippet.rb
+++ ruby/snippet.rb
@@ -6,3 +6,8 @@ end
 def is_int_array(arr)
   arr.all? {|e| e % 1.0 == 0} rescue false
 end
+
+def up_array(arr)
+  return nil if arr.empty? || arr.any? { |x| x < 0 || x > 9 }
+  arr.join.next.chars.map(&:to_i)
+end
