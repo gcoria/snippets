393c730f8caca12727a65d02dc6bb55ebfe2dd00
Gaston <gaston.e.coria@gmail.com>
2021-10-12 22:13:44 -0300

    ip_adress

--- ruby/snippet.rb
+++ ruby/snippet.rb
@@ -15,3 +15,7 @@ end
 def title_case(title, minor_words = '')
   title.capitalize.split().map{|a| minor_words.downcase.split().include?(a) ? a : a.capitalize}.join(' ')
 end
+
+def ip_to_int32(ip)
+  x = IPAddr.new(ip).to_i
+end
